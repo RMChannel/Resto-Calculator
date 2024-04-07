49b4e9bad32658e66af9a98d9ee39dec9c722429
RMChannel <98057423+RMChannel@users.noreply.github.com>
2024-04-07 10:20:05 +0200

    Code complete

---
+++ script.js
@@ -0,0 +1,17 @@
+var confirm = new Audio("confirm.mp3");
+var error = new Audio("error.mp3");
+
+function CalcolaResto() {
+    let mat=document.getElementById("input").value;
+    if (mat<10000 || mat>9999999999) {
+        error.play();
+        alert("Valore matricola non corretto!\nInserisci o le ultime 5 cifre la tua matricola intera");
+    }
+    else {
+        mat=mat%100000;
+        mat=mat%3;
+        document.getElementById("output").textContent="Sei resto: "+mat;
+        document.getElementById("output").style.display="block";
+        confirm.play();
+    }
+}
